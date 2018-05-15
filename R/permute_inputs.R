283bce5dc605bda736fd2a650417adac8537bcc5
Edwin <edwin_farley@brown.edu>
2018-05-15 11:32:04 -0400

    First commit

---
+++ R/permute_inputs.R
@@ -0,0 +1,61 @@
+#' Runs the permutation sampling procedure on the two datasets specified by the
+#' df1_path and df2_path inputs over the blocks defined in the datasets.
+#'
+#' @param df1_path A string with the path (from the R working directory) to the
+#' first dataset with blocks.
+#' @param df2_path A string with the path (from the R working directory) to the
+#' 			second dataset with blocks.
+#' @param formula A string with the formula specifying the response variable
+#' 			and the covariates. Written in the R format (e.g. 'y ~ x1 + x2 + x3').
+#' @param family A string specifying the family of distributions used in the
+#' 			regression step. Current supported families are 'Normal', 'Logistic',
+#'			"Poisson".
+#' @param N An integer specifying the number of desired full permutations. The
+#' 			number of iterations will be burn_in + N*sample_interval.
+#' @param I An integer specifying how many iterations to complete in sampling
+#' 			the regression parameters.
+#' @param t An integer specifying the number of iterations (successful or not)
+#' 			in the Metropolis-Hastings step.
+#' @param burn_in An integer specifying the number of full iterations to be
+#' 			completed before any samples are taken.
+#' @param sample_interval An integer specifying the number of full iterations
+#' 			in between each sample.
+#' @param conda_env 'NA' by default. If using the Anaconda distribution of
+#' 			python, specify which environment to use. Runs 'source activate conda_env'.
+#' @param activate_env 'NA' by default. Enter a string with the command used to
+#'			activate the desired python environment if it neither the default
+#'			environment nor an Anaconda environment.
+#' @param keep_csv FALSE by default. If TRUE, the .csv file containing the
+#'			permutations will not be automatically deleted upon completion of the
+#'			procedure.
+#' @return A dataframe with number of rows equal to the number of rows in the
+#' 			first dataset and N columns. Each column is a full permutation with
+#' 			respect to the first dataset to be applied to the second dataset.
+#' @export permute_inputs
+
+permute_inputs = function(df1_path, df2_path, formula, family, N, I, t, burn_in, sample_interval, conda_env = 'NA', activate_env = 'NA', keep_csv = FALSE){
+	fileConn<-file("input.txt")
+	writeLines(c(df1_path, df2_path, formula, family, toString(N), toString(I),
+	 	toString(t), toString(burn_in), toString(sample_interval)), fileConn)
+	close(fileConn)
+	package_path = path.package('GFSsampler')
+	exec_file = paste(package_path, '/python/exec.py', sep = '')
+	command = ''
+
+	if(conda_env != 'NA'){
+		command = paste('source activate', conda_env, ';', sep = ' ')
+	} else if(activate_env != 'NA'){
+		command = paste(activate_env, ';', sep = ' ')
+		}
+
+	command = paste(command, 'python', exec_file, package_path, getwd(), sep = ' ')
+	system(command)
+
+	perms = read.csv('permutations.csv')
+	perms$X = NULL
+	system('rm input.txt')
+	system(paste('rm -r ', package_path, '/python/__pycache__', sep = ''))
+	if(! keep_csv){system('rm permutations.csv')}
+
+	return(perms+1)
+}
---
+++ R/py_setup.R
@@ -0,0 +1,11 @@
+#' Installs the required python files to the package directory.
+#' No input is required. Python code can be found on GitHub at:
+#' https://github.com/edwinfarley/GFS
+#'
+#' @export py_setup
+
+py_setup = function(){
+    package_path = paste(path.package('GFS'), '/python', sep ='')
+    system(paste('rm -r', package_path, sep = ' '))
+    system(paste('git clone https://github.com/edwinfarley/GFSpython', package_path, sep = ' '))
+}
