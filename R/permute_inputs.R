719106d5fea716570086422b67a8466dc32bdb2d
Edwin <edwin_farley@brown.edu>
2020-01-24 10:00:54 -0500

    01/24/20

--- R/permute_inputs.R
+++ R/permute_inputs.R
@@ -25,26 +25,33 @@
 #'			sample_interval is 1, every full iteration will be a sample, if
 #'			sample_interval is 2, every other full iteration will be a sample,
 #'          and so on.
+#' @param block_name The name of the column in the two input data sets that
+#'          contains the number of the block to which each row belongs. Set to
+#'          "block" by default. Column name must be the same in the two data sets.
 #' @param conda_env 'NA' by default. If using the Anaconda distribution of
 #' 			python, specify which environment to use. Runs 'source activate conda_env'.
 #' @param activate_env 'NA' by default. Enter a string with the command used to
 #'			activate the desired python environment if it is neither the default
 #'			environment nor an Anaconda environment.
-#' @param keep_csv FALSE by default. If TRUE, the .csv file containing the
-#'			permutations will not be automatically deleted upon completion of the
-#'			procedure.
+#' @param python The name of the installed python deployment. If a conda environment
+#'			was set up with create_conda_environment, this should be "python".
 #' @return A dataframe with number of rows equal to the number of rows in the
 #' 			first data set and N columns. Each column is a full permutation with
 #' 			respect to the first data set to be applied to the second data set.
 #' @export permute_inputs
+#' @importFrom utils read.csv
+#' @importFrom stringr str_replace
 
-permute_inputs = function(df1_path, df2_path, formula, family, N, I, t, burn_in, sample_interval, block_name = 'block', conda_env = 'NA', activate_env = 'NA', python = "python", keep_csv = FALSE){
+permute_inputs = function(df1_path, df2_path, formula, family, N, I, t, burn_in, sample_interval, block_name = 'block', conda_env = 'NA', activate_env = 'NA', python = "python"){
+	# Get path of package. The python code will be kept here.
 	package_path = path.package('GFS')
+	# Get path of session temporary files. Need to replace double backslash in path.
+	temp_path = str_replace(tempdir(), "//", "/")
 	exec_file = paste(package_path, '/python/exec.py', sep = '')
 
 	if(!file.exists(exec_file)){py_setup()}
-
-	fileConn<-file("input.txt")
+	input_path = paste(temp_path, "/input.txt", sep = "")
+	fileConn = file(input_path)
 	writeLines(c(df1_path, df2_path, formula, family, toString(N), toString(I),
 	 	toString(t), toString(burn_in), toString(sample_interval), block_name), fileConn)
 	close(fileConn)
@@ -57,16 +64,19 @@ permute_inputs = function(df1_path, df2_path, formula, family, N, I, t, burn_in,
 		command = paste(activate_env, ';', sep = ' ')
 		}
 	killed = FALSE
-	command = paste(command, python, exec_file, package_path, getwd(), sep = ' ')
+	command = paste(command, python, exec_file, package_path, getwd(), temp_path, sep = ' ')
 	
 	tryCatch(system(command), interrupt = print("Process Start."), finally = system("pkill python"))
-	perms = read.csv('permutations.csv')
+	csv_path = paste(temp_path, "/permutations.csv", sep = "")
+	perms = read.csv(csv_path)
 	perms$X = NULL
-	system('rm input.txt')
-	system(paste('rm -r ', package_path, '/python/__pycache__', sep = ''))
-	if(! keep_csv){system('rm permutations.csv')}
+	# Remove the inputs file
+	system(paste("rm ", input_path, sep = ""))
+	# Clear python cache
+	system(paste("rm -r ", package_path, "/python/__pycache__", sep = ""))
+	# Remove .csv file
+	system(paste("rm ", csv_path, sep = ""))
 
 	perms[perms == -1] = NA
 	return(perms+1)
-	
 }
