82d582ca87f8752274b32122476ad5b487a09302
Edwin <edwin_farley@brown.edu>
2020-01-24 10:56:14 -0500

    01/24/20

--- R/py_setup.R
+++ R/py_setup.R
@@ -1,13 +1,19 @@
 #' Installs the required python files to the package directory.
-#' No input is required. Python code can be found on GitHub at:
+#' No input is required, but the force argument can be set. 
+#' Python code can be found on GitHub at:
 #' https://github.com/edwinfarley/GFSpython
 #'
+#' @param force Whether to remove existing python directory and re-install components.
 #' @export py_setup
 
-py_setup = function(){
-    package_path = paste(path.package('GFS'), '/python', sep ='')
-    if(!file.exists(package_path)){
-        system(paste('rm -r', package_path, sep = ' '))
-        system(paste('git clone https://github.com/edwinfarley/GFSpython', package_path, sep = ' '))
+py_setup = function(force = FALSE){
+    python_path = paste(path.package('GFS'), '/python', sep ='')
+    if(!file.exists(python_path)){
+        system(paste('git clone https://github.com/edwinfarley/GFSpython', python_path, sep = ' '))
+    } else if(force) {
+    		system(paste('rm -r', python_path, sep = ' '))
+    		system(paste('git clone https://github.com/edwinfarley/GFSpython', python_path, sep = ' '))
+    } else {
+    		print("Python components already installed. Set force to TRUE to re-install python components.")
     }
 }
