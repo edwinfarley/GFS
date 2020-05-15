885ad71ec1f891be89aaeab7c86ffe41958481e9
Edwin <edwin_farley@alumni.brown.edu>
2020-05-14 21:54:22 -0700

    05/15/20

---
+++ R/samples1.R
@@ -0,0 +1,19 @@
+#' Partition 1 of example subset of the Behavioral risk factor surveillance system survey data set (BRFSS)
+#' consisting of 3200 rows extracted from the full data set. Entries sampled at random from
+#' complete rows (no missing or warning values).
+#'
+#' @source Department of Health, Centers for Disease Control Human Services, and Pre-
+#' vention (CDC). Behavioral risk factor surveillance system survey data, 2001.
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{PHYSHLTH}{A measure of physical health on a scale of 1 to 30.}
+#'  \item{MENTHLTH}{A measure of mental health on a scale of 1 to 30.}
+#'  \item{AGE}{The respondent's age.}
+#'  \item{WEIGHT}{The respondent's weight in lbs.}
+#'  \item{block}{Blocking index.}
+#' }
+#' @examples
+#' \dontrun{
+#'  samples1
+#' }
+"samples1"
---
+++ R/samples2.R
@@ -0,0 +1,18 @@
+#' Partition 2 of example subset of the Behavioral risk factor surveillance system survey data set (BRFSS)
+#' consisting of 3200 rows extracted from the full data set. Entries sampled at random from
+#' complete rows (no missing or warning values).
+#'
+#' @source Department of Health, Centers for Disease Control Human Services, and Pre-
+#' vention (CDC). Behavioral risk factor surveillance system survey data, 2001.
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{GENHLTH}{A measure of general health on a scale of 1 to 5.}
+#'  \item{ALCDAY}{The number of days a year the respondent drinks alcohol.}
+#'  \item{ASTHMA}{Whether the respondent has asthma.}
+#'  \item{block}{Blocking index.}
+#' }
+#' @examples
+#' \dontrun{
+#'  samples2
+#' }
+"samples2"
