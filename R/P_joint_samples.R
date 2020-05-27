030f5794ce853e2dee0a2efda05288eed8974d7e
Edwin <edwin_farley@alumni.brown.edu>
2020-05-26 18:34:02 -0700

    05/26/20

---
+++ R/P_joint_samples.R
@@ -0,0 +1,12 @@
+#' Example output of permute_inputs to be used with samples1 and samples2 data.
+#' Permutations samples using joint model (Normal + Logistic).
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'   Each column is a permutation.
+#' }
+#' @examples
+#' \dontrun{
+#'  P_joint_samples
+#' }
+"P_joint"
---
+++ R/P_samples.r
@@ -0,0 +1,12 @@
+#' Example output of permute_inputs to be used with samples1 and samples2 data.
+#' Permutations samples using Normal model.
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'   Each column is a permutation.
+#' }
+#' @examples
+#' \dontrun{
+#'  P_samples
+#' }
+"P"
